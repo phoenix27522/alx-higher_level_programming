0c7ae9e850ee1fff2b8f253befdf9570efd4ca5e
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-07 15:15:15 +0300

    fix of the printing list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    for l in range(len(my_list)):
-        print("{:d}".format(l))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
