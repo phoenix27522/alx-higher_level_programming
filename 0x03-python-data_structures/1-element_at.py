d7447595b0c41ce29a85b46d6d50b024ddab2e34
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-07 15:22:09 +0300

    fix of the retriving an element from list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def element_at (my_list, idx):
+def element_at(my_list, idx):
     for i in range(len(my_list)):
         if idx < 0 or idx > len(my_list) - 1:
             return 'None'
