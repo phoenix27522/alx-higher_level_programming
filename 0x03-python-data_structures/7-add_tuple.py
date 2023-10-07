5039a57f56ed2edf39e8d8bcc3abffe53296113f
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-07 20:22:57 +0300

    fix of the tuple "remeber the to modify to the old code"

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Create a list to store the added elements
-    result = []
-
-    # Determine the length of each tuple
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-
-    # Determine the maximum length between the two tuples
-    max_length = max(len_a, len_b)
-
-    for i in range(max_length):
-        a_value = tuple_a[i] if i < len_a else 0
-
-        b_value = tuple_b[i] if i < len_b else 0
-        # Add the elements together
-        add = a_value + b_value
-        result.append(add)
-
-    return tuple(result)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
