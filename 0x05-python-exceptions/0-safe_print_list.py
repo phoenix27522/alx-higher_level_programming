da325f6e4bab396944416f1489722cc8bcd5fda6
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-23 14:03:48 +0300

    handeling exception for out range by ignoring the error and and printing the whole list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             len_l += 1
-
     except IndexError:
         pass
 
