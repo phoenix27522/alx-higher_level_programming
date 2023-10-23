13050efd99f2f16ea2dbfca8504ec106a2d9f997
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-23 20:38:28 +0300

    upgraded function that handels except with its msg

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+import sys  # To access stderr
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+
