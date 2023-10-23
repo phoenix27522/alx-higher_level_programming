2fc7e18a37e7a819abc34d4b7c360b16f9e33c91
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-23 15:33:16 +0300

    handdeling the exception for value is not an int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        int_value = int(value)
+        print(value);
+        return True
+    except (ValueError, TypeError):
+        return False
