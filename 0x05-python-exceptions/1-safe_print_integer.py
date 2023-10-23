bd7de5cc456af3ce5091bbf5a52b2c9104588c9d
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-23 15:34:54 +0300

    handdeling the exception for value is not an int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         int_value = int(value)
-        print(value);
+        print(value)
         return True
     except (ValueError, TypeError):
         return False
