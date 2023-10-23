ec99386dc180ba992cccad16f795f82bea949af4
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-23 16:12:38 +0300

    printing only int if there ar other type ignore

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    n = 0;
+
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            n += 1
+
+    except (ValueError, TypeError):
+        pass
+
+    print()
+    return n
