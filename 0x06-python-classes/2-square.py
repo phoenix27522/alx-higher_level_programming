07da997e4365ae0a42bd69bdf4a9c2cbb60056b3
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-24 14:27:31 +0300

    raising exception for the square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" a class representing a square """
+
+class Square:
+    """ square - defins its size
+        args : 
+             size: size of the square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
