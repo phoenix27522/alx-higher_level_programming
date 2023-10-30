8ec51101058a6963bacddce58c0dd030b5884e31
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-30 15:19:24 +0300

    area and paramater of rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,4 +62,6 @@ class Rectangle:
 
     def perimeter(self):
         """ returns the parameter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return ((2 * self.__width) + (2 * self.__height))
