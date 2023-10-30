77240780459fa33e714fa12178799a7a513f7958
Nahom kiflu <kiflunahom1994@gmail.com>
2023-10-30 17:13:32 +0300

    rectangle represented in a #

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -76,5 +76,6 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 rectangle_str += "#"
-            rectangle_str += "\n"
+            if i != self.__height - 1:
+                rectangle_str += "\n"
         return rectangle_str
