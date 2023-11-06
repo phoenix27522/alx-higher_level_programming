b56a884cffacb875a84828411fe9a0ccd269318d
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-06 20:54:33 +0300

    square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,4 +7,5 @@ class Square(Rectangle):
     """ square that inherits from rectangle """
     def __init__(self, size):
         """ Initialize a new Square"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
