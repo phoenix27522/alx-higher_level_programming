f9399369c10851aefcdaef6e28df8da0bfc97dee
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-06 21:32:58 +0300

    fixed task -1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,7 @@ class MyList(list):
     """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """Print the list in sorted (ascending) order."""
+        sorted_list = sorted(self)
+        print(sorted_list)
+
