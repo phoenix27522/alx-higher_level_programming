e8dce6c6c97823c9610fcdcdb7a5837ab29ed6ef
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-06 17:59:31 +0300

    area-> only rise exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#1/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+
+class BaseGeometry:
+    """ contains area function """
+
+    def area(self):
+        """ raise exception """
+        raise Exception("area() is not implemented")
