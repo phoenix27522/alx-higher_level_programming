50d1c87d3b43419619444e45b048f602253ba391
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-06 16:33:26 +0300

    is instance of an object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" checking an object have the same instance """
+
+
+def is_same_class(obj, a_class):
+    """ function that check if an object has the same instance """
+    return type(obj) is a_class
