f9b64ea31c4836a40263e84e8b64416bd075fb4b
Nahom kiflu <kiflunahom1994@gmail.com>
2023-11-09 13:56:04 +0300

    rectangle with string format

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,8 @@ class Rectangle(Base):
 
         for i in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """ returns in string format """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
