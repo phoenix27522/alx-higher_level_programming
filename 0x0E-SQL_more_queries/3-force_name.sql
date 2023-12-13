dd5c4b9bf68afe9922828bf6b416b1fec5386bd9
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-13 14:34:43 +0300

    fix on the task 4

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,2 @@
--- Check if the table exists first
-SELECT COUNT(*)
-FROM information_schema.tables
-WHERE table_schema = DATABASE() 
-AND table_name = 'force_name';
-
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
