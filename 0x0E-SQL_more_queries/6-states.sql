232676103cf155874e29d4f58d015f9c876f3809
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-13 20:23:09 +0300

    creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL serve

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,12 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Use the database
 USE hbtn_0d_usa;
-
--- Check if the table exists
-SELECT COUNT(*)
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0d_usa' 
-AND table_name = 'states';
-
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id), UNIQUE (id));
