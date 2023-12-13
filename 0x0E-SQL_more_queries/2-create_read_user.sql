91d463368fed289d827370adc0e7053f0f85d7cf
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-13 14:14:59 +0300

    create data base and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,8 +7,10 @@ FROM mysql.user
 WHERE user = 'user_0d_2';
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
