ffd2f537c90953ae4143210839b5acdc55d9c7c3
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-13 14:02:37 +0300

    create user in mysql server

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,13 @@
+-- Check if the user exists first
 SELECT COUNT(*)
 FROM mysql.user
 WHERE user = 'user_0d_1';
 
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
 
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
