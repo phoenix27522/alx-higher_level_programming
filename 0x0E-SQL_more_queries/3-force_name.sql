0c0e3d5e2f98fab9c980dee6aa9a754711e329fe
Nahom kiflu <kiflunahom1994@gmail.com>
2023-12-13 14:21:26 +0300

    creates the table force_name on your MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,13 @@
+USE your_database_name; -- Replace 'your_database_name' with your actual database name
+
+-- Check if the table exists first
+SELECT COUNT(*)
+FROM information_schema.tables
+WHERE table_schema = DATABASE() 
+AND table_name = 'force_name';
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
