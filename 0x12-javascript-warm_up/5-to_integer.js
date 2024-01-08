dee3e2621f4748e04a8f2f2bf5ef987c1aa2f652
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-08 18:07:58 +0300

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Not a number');
+const firstArg = process.argv[2];
+const num = parseInt(firstArg);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
 } else {
-  console.log('My number:', parseInt(process.argv[2]));
+  console.log('Not a number');
 }
