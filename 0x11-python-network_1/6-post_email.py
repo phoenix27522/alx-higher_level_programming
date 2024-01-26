89f880498f618c4b09efd6d1922c32bfafee0ecc
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-26 23:48:12 +0300

    task 6 fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with an email 
+"""Sends a POST request to a given URL with an email
    parameter and displays the response.
 """
 import sys
