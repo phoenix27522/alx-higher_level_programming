89152ef57e5e62d000b8efd5d6cd92c4a79f390a
nahom <kiflunahom1994@gmail.com>
2024-01-26 13:09:25 -0800

    fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter."""
+"""Sends a POST request to http://0.0.0.0:5000/search_user
+   with a letter as a parameter.
+"""
 import sys
 import requests
 
