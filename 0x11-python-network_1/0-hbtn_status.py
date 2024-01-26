4f4cb5a0076d24cedb2272f4bed18a7b17b69c56
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-26 13:17:56 +0300

    task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status"""
-
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-# Use a with statement to open the URL
-with urllib.request.urlopen(url) as response:
-    # Read the content of the response
-    content = response.read().decode('utf-8')
-
-    # Display the body of the response
-    print(f"Body response:\n\t- type: {type(content)}\n\t- content: {content}")
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
