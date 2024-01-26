9bf3714e74b0e32643cb96805ab06abfe01c0f6c
nahom <kiflunahom1994@gmail.com>
2024-01-26 12:43:48 -0800

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with an email parameter and displays the response."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print(response.text)
+
